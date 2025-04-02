93205b91f4f684633eb60afcc98db0f6ab11d4a8
master <ted3131@diquest.com>
2025-04-03 08:12:48 +0900

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
-
 import "./App.css";
+import Home from "./pages/Home";
+import Diary from "./pages/Diary";
+import New from "./pages/New";
+import { Route, Routes, Link, useNavigate } from "react-router-dom";
+import Notfound from "./pages/Notfound";
+import { getEmotionImage } from "./util/get-emotion-images";
 
+//1. "/" : 모든 일기를 조회하는 home 페이지
+//2. "/new " 새로운 일기를 작성하는 new 페이지
+//3. "/diary" 일기를 상세히 조회하는 diary 페이지지
 function App() {
+  const nav = useNavigate();
 
-  return <></>;
+  const onClickButton = () => {
+    nav("/new");
+  };
+
+  return (
+    <>
+      <div>
+        <img src={getEmotionImage(1)} />
+        <img src={getEmotionImage(2)} />
+        <img src={getEmotionImage(3)} />
+        <img src={getEmotionImage(4)} />
+        <img src={getEmotionImage(5)} />
+      </div>
+      <div>
+        <Link to={"/"}> Home </Link>
+        <Link to={"/new"}> New </Link>
+        <Link to={"/diary"}> Diary </Link>
+      </div>
+      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary/:id" element={<Diary />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
